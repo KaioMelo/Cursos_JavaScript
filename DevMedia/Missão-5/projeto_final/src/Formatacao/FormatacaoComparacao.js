d1729f826a36b9893acb9409e5d55203e9a42ba3
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-06-12 20:12:09 -0300

    Subindo ultimos ajustes do projeto

--- DevMedia/Missão-5/projeto_final/src/Formatacao/FormatacaoComparacao.js
+++ DevMedia/Missão-5/projeto_final/src/Formatacao/FormatacaoComparacao.js
@@ -1,16 +1,38 @@
-import ListaDeSalario from "../Listas/ListaDeSalarios.js"
-import ListaDeInflacao from "../Listas/ListaDeInflacao.js"
+import ListaDeSalario from "../Listas/ListaDeSalarios.js";
+import ListaDeInflacao from "../Listas/ListaDeInflacao.js";
 
 const Comparacao = () => {
-    for(let i in ListaDeSalario){
-        let ano = ListaDeSalario[i].ano
-        let salario = ListaDeSalario[i].salarios
-        let diferenca = salario- salario
-        let crescimento = (diferenca / salario) * 100
-        console.log(`Ano: ${ano}`)
-        console.log(`Salário mínimo: ${salario}`)
-        console.log(`Crescimento Salarial: ${crescimento}`)
-    }
-}
+  let salarioAnterior = "0";
+  for (let i in ListaDeSalario) {
+    let ano = String(ListaDeSalario[i].ano).padStart(30, ".");
+    let salario = ListaDeSalario[i].salarios;
+    let salarioFormatado = salario
+      .toFixed(2)
+      .replace(".", ",")
+      .padStart(19, ".");
 
-export default Comparacao
+    console.log();
+    console.log(`Ano: ${ano}`);
+    console.log(`Salário mínimo: ${salarioFormatado}`);
+
+    if (salarioAnterior != 0) {
+      let diferenca = salario - salarioAnterior;
+      let crescimento = (diferenca / salarioAnterior) * 100;
+      let crescimentoFormatado = crescimento
+        .toFixed(2)
+        .replace(".", ",")
+        .padStart(12, ".");
+      console.log(`Crescimento Salarial: ${crescimentoFormatado}%`);
+    } else {
+      console.log(`Crescimento Salarial: -`);
+    }
+    let ipca = ListaDeInflacao[i].ipca;
+    let ipcaFormatado = ipca.toFixed(2).replace(".", ",").padStart(19, ".");
+    console.log(`Inflação IPCA: ${ipcaFormatado}%`);
+    console.log();
+
+    salarioAnterior = salario;
+  }
+};
+
+export default Comparacao;
